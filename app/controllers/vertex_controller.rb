c3b14f0ce8475709f3ce7ae80ddbb3a3ba9b29e7
Ron Kandah <ronkandah@gmail.com>
2016-01-25 13:23:19 -0800

    changed amount returned to 0.00 to test https://coupa.zendesk.com/agent/tickets/96994

--- app/controllers/vertex_controller.rb
+++ app/controllers/vertex_controller.rb
@@ -14,7 +14,7 @@ def create
             <tax-lines>
               <tax-line>
                 <line-num>1</line-num>
-                <tax-engine-line>99999999.00</tax-engine-line>
+                <tax-engine-line>0.00</tax-engine-line>
               </tax-line>
             </tax-lines>
           </invoice-line>
@@ -25,21 +25,21 @@ def create
     hdr_xml = %Q|
       <?xml version="1.0" encoding="UTF-8"?>
       <invoice-header>
-        <tax-engine-header>99999999.00</tax-engine-header>
+        <tax-engine-header>0.00</tax-engine-header>
       </invoice-header>
       |.strip
 
     qa_xml = %Q|
       <?xml version="1.0" encoding="UTF-8"?>
       <invoice-header>
-        <tax-engine-header>99999999.00</tax-engine-header>
+        <tax-engine-header>0.00</tax-engine-header>
         <invoice-lines>
           <invoice-line>
             <line-num>1</line-num>
             <tax-lines>
               <tax-line>
                 <line-num>1</line-num>
-                <tax-engine-line>99999999999</tax-engine-line>
+                <tax-engine-line>0</tax-engine-line>
               </tax-line>
             </tax-lines>
           </invoice-line>
